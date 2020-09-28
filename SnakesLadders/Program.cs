795d25bbb38a2190cd6fde637ba2dc844f861cb6
Rachit Rathi <rachitrathi98@gmail.com>
2020-09-28 19:22:54 +0530

    First Commit

--- SnakesLadders/Program.cs
+++ SnakesLadders/Program.cs
@@ -6,9 +6,23 @@ namespace SnakesLadders
     {
         static void Main(string[] args)
         {
-            int position = 0;
+            int position = 0 ;
             Random random = new Random();
             int diceNum = random.Next(1, 7);
+            int option = random.Next(1, 4);
+            if (option == 1)// No Play
+            {
+                position = 0;
+            }
+            else if (option == 2)// Ladder
+            {
+                position += diceNum;
+            }
+            else //Snake
+            {   
+                    position -= diceNum;
+                
+            }
         }
     }
 }
