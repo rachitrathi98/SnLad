7091a5213da8653d60758d1d09270cef09f6418a
Rachit Rathi <rachitrathi98@gmail.com>
2020-09-28 19:26:40 +0530

    First Commit

--- SnakesLadders/Program.cs
+++ SnakesLadders/Program.cs
@@ -6,23 +6,53 @@ namespace SnakesLadders
     {
         static void Main(string[] args)
         {
-            int position = 0 ;
+            int position = 0;
+            int diceCount = 0;
+            int diceNum;
+            int option;
             Random random = new Random();
-            int diceNum = random.Next(1, 7);
-            int option = random.Next(1, 4);
-            if (option == 1)// No Play
+            while (position <= 100)
             {
-                position = 0;
+                diceNum = random.Next(1, 7);
+                diceCount++;
+                option = random.Next(1, 4);
+                if (option == 1)// No Play
+                {
+                    position += 0;
+                }
+                if (option == 2)// Ladder
+                {
+                    if (position + diceNum <= 100)
+                    {
+                        position += diceNum;
+                    }
+                    else
+                    {
+                        position += 0;
+                    }
+                }
+                else //Snake
+                {
+                    if (position - diceNum > 0)
+                    {
+                        position -= diceNum;
+                    }
+                    else
+                    {
+                        position = 0;
+                    }
+
+                }
+                Console.WriteLine("The final position is " + position);
             }
-            else if (option == 2)// Ladder
-            {
-                position += diceNum;
-            }
-            else //Snake
-            {   
-                    position -= diceNum;
-                
-            }
+
         }
+
     }
 }
+
+            
+        }
+    }
+
+
