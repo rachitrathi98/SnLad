6b10b5d89599bd4fa5e2bc02a6921948e4188e25
Rachit Rathi <rachitrathi98@gmail.com>
2020-09-28 18:24:25 +0530

    First Commit

--- SnakesLadders/Program.cs
+++ SnakesLadders/Program.cs
@@ -7,6 +7,8 @@ namespace SnakesLadders
         static void Main(string[] args)
         {
             int position = 0;
+            Random random = new Random();
+            int diceNum = random.Next(1, 7);
         }
     }
 }
